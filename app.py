0c0d2f8d92472e4f95fb3a423558f49e950e61ef
palistha.deshar <palistha.deshar@tai.com.np>
2024-02-20 22:22:17 +0545

    t5-model

--- app.py
+++ app.py
@@ -4,17 +4,17 @@ import random
 from functions import *
 from transformers import pipeline
 from transformers import AutoTokenizer, AutoModelForQuestionAnswering
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
 from huggingface_hub import notebook_login
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
+from transformers import T5ForConditionalGeneration, T5TokenizerFast
 
 
 # notebook_login()
 
 
-modelPath = "sentence-transformers/all-mpnet-base-v2"
+modelPath = "sentence-transformers/multi-qa-mpnet-base-dot-v1"
 encode_kwargs = {'normalize_embeddings': False}
 embeddings = HuggingFaceEmbeddings(
     model_name=modelPath,
@@ -22,27 +22,9 @@ embeddings = HuggingFaceEmbeddings(
 )
 save_directory = "vector_db"
 database = FAISS.load_local(save_directory,embeddings)
-model_path = "Palistha/finetuned-gpt2"
-tokenizer="Palistha/finetuned-gpt2"
+model_path = "Palistha/finetuned-t5-small"
+tokenizer = "Palistha/finetuned-t5-small"
 max_len=1024
-# query = "Who is the CEO of TAI?"
-
-    
-
-#output
-input_text = '''Mission and Vision | TAI 日本語 Contact OUR STORY ABOUT US Introduction Message from CEO Team Service About Nepal SDGs BLOG English 日本語 Contact About TAI Vision Smiling Nepal TAI was established with the vision of “Smiling Nepal”. TAI is dedicated in assisting hardworking and versatile Nepali IT graduates in contributing to their country via Technology and Intelligence. We are looking forth to becoming an IT bridge between Nepal and Japan. Mission Accelerate the GDP of Nepal by 1%. We want to create highly skilled jobs in our own country and stop brain-drain of the nation. Our motto is to "Serve Nation With Innovation". Our ultimate goal is to become an offshore company large enough to accelerate the GDP of Nepal by 1%. Company Info Company Name:TAI Inc (Technology and Intelligence) Established:Nepal (2020), Japan (2022) CEO:Sharad Rai Main Bank: Mitsubishi UFJ Bank PayPay Bank Capital:1 million yen (30 million yen including Nepal Group) Employees:70 (Including Nepal and Japan Group employees) Address: Matsumoto Building-502, 10 Iwato-cho, Shinjuku-ku, Tokyo, Japan Kathmandu, Shantinagar, Nepal Company Services Contact Blog Services Artificial Intelligence Digital Transformation Cloud & DevOps Web Application Development Mobile Application Development UI/UX Design Connect With Us Address Matsumoto Building-502, 10 Iwato-cho, Shinjuku-ku, Tokyo, Japan SanoKharibot Shantinagar, Kathmandu, Nepal Copyright 2024 TAI Inc. All rights reserved. Privacy Policy Terms of Use ''' + "What is the mission of TAI?"
-# # input_text = context + " " + query
-# model_path = "Palistha/finetuned-gpt2"
-# tokenizer="Palistha/finetuned-gpt2"
-# seq = input_text
-# max_len=1024
-# output_text = generate_text(model_path,tokenizer,seq,max_len)
-# parts = output_text.split(input_text)
-# answer = parts[-1].strip()
-# print("Answer:", answer)
-
-
-
 
 # Display chat messages from history on app rerun
 if 'messages' not in st.session_state:
@@ -64,9 +46,8 @@ if query:
 
 
     @st.cache_resource(show_spinner=True)
-    def load_data():
+    def load_data(query):
         search = database.similarity_search(query,k=1)
-        print(search)
         for doc in search:
             context = doc.page_content
             source = doc.metadata['source']
@@ -75,12 +56,17 @@ if query:
 
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
-            context,source = load_data()
-            input_text = context + " " + query
-            output_text = generate_text(model_path,tokenizer,input_text,max_len)
-            parts = output_text.split(input_text)
-            answer = parts[-1].strip()
-            output = answer + " "  + "\n Source:" + source
+            context,source = load_data(query)
+            if not context.endswith("."):
+                context += "."
+            if not query.endswith("?"):
+                query += "?"
+            answer = generate_answer(model_path,context,query)
+            try:
+                if math.isnan(float(answer)):
+                    output =  "Sorry, I could not generate a valid answer."
+            except (ValueError, TypeError):
+                output = answer + " "  + "\n Source:" + source
             st.write(output)
             st.session_state.messages.append({"role": "assistant", "content": output})
 
--- functions.py
+++ functions.py
@@ -2,6 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 import re
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
+from transformers import T5ForConditionalGeneration, T5TokenizerFast
+import math
+
 
 
 
@@ -69,25 +72,29 @@ def dataset(filtered_url,data:list):
 
 
 def load_model(model_path):
-    model = GPT2LMHeadModel.from_pretrained(model_path)
+    model = T5ForConditionalGeneration.from_pretrained(model_path)
     return model
 
 
 def load_tokenizer(tokenizer_path):
-    tokenizer = GPT2Tokenizer.from_pretrained(tokenizer_path)
+    # tokenizer = GPT2Tokenizer.from_pretrained(tokenizer_path)
+    tokenizer = T5TokenizerFast.from_pretrained(tokenizer_path)
     return tokenizer
 
-def generate_text(model_path,tokenizer, sequence, max_length):
 
+def generate_answer(model_path,context, question):
     model = load_model(model_path)
-    tokenizer = load_tokenizer(tokenizer)
-    ids = tokenizer.encode(f'{sequence}', return_tensors='pt')
+    tokenizer = load_tokenizer(model_path)
+    prompt = context + " " + question
+    ids = tokenizer.encode(f'{prompt}', return_tensors="pt")
     final_outputs = model.generate(
         ids,
         do_sample=True,
-        max_length=max_length,
+        max_length=1024,
         pad_token_id=model.config.eos_token_id,
         top_k=50,
         top_p=0.95,
     )
-    return tokenizer.decode(final_outputs[0], skip_special_tokens=True)
+    answer = tokenizer.decode(final_outputs[0], skip_special_tokens=True)
+    return answer
+   
