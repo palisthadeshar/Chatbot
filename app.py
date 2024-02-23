b97de7b3491a1e64adc91dd15acededf2373e2d7
palistha.deshar <palistha.deshar@tai.com.np>
2024-02-23 09:42:19 +0545

    similarity search modified

--- app.py
+++ app.py
@@ -2,29 +2,33 @@ import streamlit as st
 from streamlit_chat import message
 import random
 from functions import *
-from transformers import pipeline
-from transformers import AutoTokenizer, AutoModelForQuestionAnswering
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
-from huggingface_hub import notebook_login
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
-from transformers import T5ForConditionalGeneration, T5TokenizerFast
+import cohere
 
 
-# notebook_login()
+api_key = "vEjPvxBmPghBYYCtAwkEtuDbWQzib8iMtLA7lLwX"
+co = cohere.Client(api_key)
 
+# modelPath = "sentence-transformers/all-mpnet-base-v2"
+# encode_kwargs = {'normalize_embeddings': False}
+# embeddings = HuggingFaceEmbeddings(
+#     model_name=modelPath,
+#     encode_kwargs=encode_kwargs
+# )
+# save_directory = "vector_db"
+# database = FAISS.load_local(save_directory,embeddings)
 
-modelPath = "sentence-transformers/multi-qa-mpnet-base-dot-v1"
-encode_kwargs = {'normalize_embeddings': False}
-embeddings = HuggingFaceEmbeddings(
-    model_name=modelPath,
-    encode_kwargs=encode_kwargs
-)
-save_directory = "vector_db"
-database = FAISS.load_local(save_directory,embeddings)
 model_path = "Palistha/finetuned-t5-small"
 tokenizer = "Palistha/finetuned-t5-small"
-max_len=1024
+
+# model_path = "Palistha/finetuned-t5-base"
+# tokenizer = "Palistha/Finetuned-t5-base"
+# max_len=1024
+with open("data/file.txt", 'r') as file:
+    lst = file.readlines()
+    data = [item.strip() for item in lst]
+
 
 # Display chat messages from history on app rerun
 if 'messages' not in st.session_state:
@@ -39,7 +43,6 @@ query = st.chat_input("Ask something")
 if query:
     with st.chat_message("user"):
         st.write(query)
-    # message(prompt, is_user=True)
     
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "user", "content": query})
@@ -47,21 +50,37 @@ if query:
 
     @st.cache_resource(show_spinner=True)
     def load_data(query):
-        search = database.similarity_search(query,k=1)
-        for doc in search:
-            context = doc.page_content
-            source = doc.metadata['source']
-        return context,source
+        response = co.rerank(
+            model = 'rerank-english-v2.0',
+            query = query,
+            documents = data,
+            top_n = 4
+            )
+        return response
+        # search = database.similarity_search(query,k=4)
+        # context = ""
+        # source = []
+        # for doc in search:
+        #     text = doc.page_content
+        #     context += text
+        #     link = doc.metadata['source']
+        #     source.append(link)
+        # return context,source
 
 
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
-            context,source = load_data(query)
+            response = load_data(query)
+            context = ""
+            for idx, r in enumerate(response):
+                context += r.document['text']
+                context += " "
             if not context.endswith("."):
                 context += "."
             if not query.endswith("?"):
                 query += "?"
             answer = generate_answer(model_path,context,query)
+            source = get_urls(context)
             try:
                 if math.isnan(float(answer)):
                     output =  "Sorry, I could not generate a valid answer."
